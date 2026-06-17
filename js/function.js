fc1616e4bf7ce0807d9aac559575ac2f2a169bd6
mariam520 <mariammagdyfayez@gmail.com>
2026-06-17 15:19:37 +0300

    edits

--- js/function.js
+++ js/function.js
@@ -403,5 +403,70 @@ new Swiper(".projectGallerySlider", {
         clickable: true,
     },
 });
+
+const swiper = new Swiper(".projectGallerySlider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        768: { slidesPerView: 1 },
+        1024: { slidesPerView: 1 }
+    }
+});
+const images = document.querySelectorAll(".gallery-img");
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+const closeBtn = document.getElementById("closeBtn");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+
+let currentIndex = 0;
+
+// open popup
+function openLightbox(index){
+    currentIndex = index;
+    lightbox.style.display = "flex";
+    lightboxImg.src = images[currentIndex].src;
+}
+
+// click image
+images.forEach((img, index)=>{
+    img.addEventListener("click", ()=>{
+        openLightbox(index);
+    });
+});
+
+// close
+closeBtn.addEventListener("click", ()=>{
+    lightbox.style.display = "none";
+});
+
+// next
+nextBtn.addEventListener("click", ()=>{
+    currentIndex = (currentIndex + 1) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+});
+
+// prev
+prevBtn.addEventListener("click", ()=>{
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+});
+
+// click outside
+lightbox.addEventListener("click", (e)=>{
+    if(e.target === lightbox){
+        lightbox.style.display = "none";
+    }
+});
 })(jQuery);
 
